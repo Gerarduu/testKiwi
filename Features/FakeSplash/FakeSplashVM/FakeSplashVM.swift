d09d1a1cdc7c94487423a266eaa6910f073f371c
Gerard <gerar.riera@gmail.com>
2021-01-23 12:32:52 +0100

    kiwi-3: - Config BaseVC, Integrate Flight Model & Test Generic API Call.
    Testing Generic API Call from FakeSplashVM.

--- Features/FakeSplash/FakeSplashVM/FakeSplashVM.swift
+++ Features/FakeSplash/FakeSplashVM/FakeSplashVM.swift
@@ -7,6 +7,30 @@
 
 import Foundation
 
+protocol FakeSplashVMDelegate: class {
+    func didLoadData(_ data: [Flight])
+    func error(_ error: Error)
+}
+
 class FakeSplashVM {
     
+    weak var delegate: FakeSplashVMDelegate?
+    
+    func loadData() {
+        APIClient.shared.requestObject(router: APIRouter.flights) { [weak self] (result: Result<FlightsRoot,Error>) in
+            guard let `self` = self else {return}
+            switch result {
+            case .failure(let err):
+                self.delegate?.error(err)
+                break
+            case .success(let value):
+                guard let data = value.data else {
+                    self.delegate?.error(AppError.generic)
+                    return
+                }
+                self.delegate?.didLoadData(data)
+                break
+            }
+        }
+    }
 }
